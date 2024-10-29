571f15f76e226346ff7d75e58167fc65242b543e
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 22:52:14 +0000

    plot random walk

---
+++ rw_visual.py
@@ -0,0 +1,13 @@
+from random_walk import RandomWalk
+from matplotlib import pyplot as plt
+
+# Make a random walk.
+rw = RandomWalk()
+rw.fill_walk()
+
+# Plot the points in the walk.
+plt.style.use('classic')
+fig, ax = plt.subplots(figsize=(15,9))
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
+
