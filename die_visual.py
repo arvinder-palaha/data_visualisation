6f885ce17cec12334d5602f47be8f1085670a593
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 23:53:33 +0000

    fixed even more typos in visualising multiplying two dice

--- die_visual.py
+++ die_visual.py
@@ -13,12 +13,12 @@ results = [die_1.roll() * die_2.roll() for _ in range(100_000)]
 # Analyze the results.
 frequencies = []
 max_result = die_1.num_sides * die_2.num_sides
-for value in range(2, max_result+1):
+for value in range(1, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.    
-x_values = list(range(3, max_result+1))
+x_values = list(range(1, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
