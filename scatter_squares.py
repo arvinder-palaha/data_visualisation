ae3a72625775c9d44102a079f0f4a969f1b7c4d9
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 22:28:08 +0000

    Use a color map that depends of the square value

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x*x for x in x_values]
 
 plt.style.use('seaborn-v0_8-whitegrid')
 fig, ax = plt.subplots()
-ax.scatter(x_values,y_values, s=10, c='red')
+ax.scatter(x_values,y_values, s=10, c=y_values, cmap=plt.cm.Blues)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
