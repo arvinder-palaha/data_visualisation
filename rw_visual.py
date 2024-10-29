46fd36cb6383b9ac470b97cac775124c5b4e1035
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 22:56:28 +0000

    keep generating random walks until user types n

--- rw_visual.py
+++ rw_visual.py
@@ -1,13 +1,19 @@
 from random_walk import RandomWalk
 from matplotlib import pyplot as plt
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots(figsize=(15,9))
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15,9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.show()
 
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
