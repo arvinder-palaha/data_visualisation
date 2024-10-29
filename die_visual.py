6226652f534751c46ad5b79802c6e4c8437369cd
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 23:47:23 +0000

    rolling three d6 die

--- die_visual.py
+++ die_visual.py
@@ -3,28 +3,29 @@ from plotly.graph_objects import Bar, Layout
 from plotly import offline
 
 # Create two dice.
-die_1 = Die(8)
-die_2 = Die(8)
+die_1 = Die(6)
+die_2 = Die(6)
+die_3 = Die(6)
 
 # Make some rolls, and store the results in a list.
 results = []
-for roll_num in range(1_000_000):
-    result = die_1.roll() + die_2.roll()
+for roll_num in range(10_000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result+1):
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+for value in range(3, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.    
-x_values = list(range(2, max_result+1))
+x_values = list(range(3, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title='Results of rolling two D8 1,000,000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
+my_layout = Layout(title='Results of rolling three D6 10,000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6_d6.html')
