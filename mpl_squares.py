eb78d9f154b54675efef74ba8e1fed43b1147106
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 22:16:18 +0000

    corrected the x axis

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-squares = [x*x for x in range(1,6)]
+input_values = [x+1 for x in range(5)]
+squares = [x*x for x in input_values]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values,squares, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
