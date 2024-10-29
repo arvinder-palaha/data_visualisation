5186cbe4f2965aace6d998d311de3d58d4a48bff
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 22:25:38 +0000

    plot a few more squares, generating the data

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [x+1 for x in range(5)]
+x_values = [x+1 for x in range(1000)]
 y_values = [x*x for x in x_values]
 
 plt.style.use('seaborn-v0_8-whitegrid')
 fig, ax = plt.subplots()
-ax.scatter(x_values,y_values, s=100)
+ax.scatter(x_values,y_values, s=10)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,5 +14,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# set range for each axis.
+ax.axis([0,1100,0,1100000])
 
 plt.show()
