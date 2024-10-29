c94b339851efcb4a6d6b6699bfee8e962741ab1f
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 23:51:16 +0000

    fixed axis range for multuplying two dice

--- die_visual.py
+++ die_visual.py
@@ -8,15 +8,12 @@ die_2 = Die(6)
 # die_3 = Die(6)
 
 # Make some rolls, and store the results in a list.
-results = []
-for roll_num in range(100_000):
-    result = die_1.roll() * die_2.roll()
-    results.append(result)
+results = [die_1.roll() * die_2.roll() for _ in range(100_000)]
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-for value in range(3, max_result+1):
+max_result = die_1.num_sides * die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
