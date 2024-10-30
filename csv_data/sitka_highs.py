a727ad893863679a4824d6d0ed172ce58ebca129
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-30 16:10:30 +0000

    extract high temperatues from the file, store in an array

--- csv_data/sitka_highs.py
+++ csv_data/sitka_highs.py
@@ -8,3 +8,12 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+        date = row[2]
+        dates.append(date)
+
+print(highs)
