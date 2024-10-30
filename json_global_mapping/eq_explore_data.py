5a381a12fe27f2edca93a1a4501206bbebfdcfb0
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-30 20:12:25 +0000

    refactored reading data from json

--- json_global_mapping/eq_explore_data.py
+++ json_global_mapping/eq_explore_data.py
@@ -14,14 +14,10 @@ with open(filename) as f:
 all_eq_data = all_eq_data['features']
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_data:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    hover_texts.append(title)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
 print(mags[:10])
 print(lons[:10])
