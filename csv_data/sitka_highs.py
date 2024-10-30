f3115f5a5a87eb0b1b409021eab88ac08f3663e7
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-30 16:24:48 +0000

    plot high temperatures of 2018

--- csv_data/sitka_highs.py
+++ csv_data/sitka_highs.py
@@ -1,4 +1,6 @@
 import csv
+import matplotlib.pyplot as plt
+
 
 filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
@@ -16,4 +18,17 @@ with open(filename) as f:
         date = row[2]
         dates.append(date)
 
-print(highs)
+# plot the high temperatures.
+print(plt.style.available)
+plt.style.use('seaborn-v0_8-darkgrid')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# format plot.
+ax.set_title("Daily high temperatures, 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
