cf7858d0ee1a54224069ac34450ce6d8c3fd2843
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-30 16:28:27 +0000

    Extract the date from the csv file and use it as the x-axis

--- csv_data/sitka_highs.py
+++ csv_data/sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 
@@ -15,19 +16,19 @@ with open(filename) as f:
     for row in reader:
         high = int(row[5])
         highs.append(high)
-        date = row[2]
+        date = datetime.strptime(row[2], '%Y-%m-%d')
         dates.append(date)
 
 # plot the high temperatures.
 print(plt.style.available)
 plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates,highs, c='red')
 
 # format plot.
 ax.set_title("Daily high temperatures, 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
-fig.autofmt_xdate()
+fig.autofmt_xdate() # automatically adjust the position of the date labels to avoid overlap
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
