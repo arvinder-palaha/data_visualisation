903fc56fc2ee7e36ab016d4b49a91ab2e4cf2c69
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-30 17:00:17 +0000

    assuming all data is filled in csv, I get an error

--- csv_data/death_valley_highs_lows.py
+++ csv_data/death_valley_highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 filename = 'data/death_valley_2018_simple.csv'
 
@@ -9,3 +10,15 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
+    # get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        print(row)
+        high = int(row[4])
+        low = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+print(highs)
