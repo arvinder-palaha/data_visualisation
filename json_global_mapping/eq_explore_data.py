14ccd6327b4aa4f351c85c7af1aa78c5718a92d8
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-30 20:03:08 +0000

    create a world map of earthquakes using plotly

--- json_global_mapping/eq_explore_data.py
+++ json_global_mapping/eq_explore_data.py
@@ -1,4 +1,8 @@
 import json
+from plotly.graph_objects import Scattergeo, Layout
+from plotly import offline
+
+
 filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
@@ -18,3 +22,12 @@ for eq_dict in all_eq_data:
     lats.append(lat)
 
 print(mags[:10])
+print(lons[:10])
+print(lats[:10])
+
+# Map the earthquakes.
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
